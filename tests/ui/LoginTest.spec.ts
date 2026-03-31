54e858f1ad8fe70544694c3f3242b02f4d4e20c9
Satnam Meena <satnam.meena2004@gmail.com>
2026-03-31 14:33:49 +0530

    initial setup

---
+++ tests/ui/LoginTest.spec.ts
@@ -0,0 +1,11 @@
+import test from "@playwright/test";
+
+
+
+test.describe("Login Tests", () => {
+
+    test("Correct username and password should go to inventory", ({ page }) => {
+           
+    })
+
+})
